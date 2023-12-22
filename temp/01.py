2b30686c3a30a1f51b420087634ad2ddb9b80d9d
fractal-robot <utsdxbur@proton.me>
2023-12-22 18:31:32 +0100

    meh v3

--- temp/01.py
+++ temp/01.py
@@ -1,8 +1,8 @@
 def checkInput(entree: str):
     """
-    entrée :
+    Entrée :
         entree (string) : la séquence ADN
-    sortie : / (goto main si séquence ADN invalide)
+    Sortie : / (goto main si séquence ADN invalide)
     """
 
     caracteresValides = set("ATGC")
@@ -12,76 +12,59 @@ def checkInput(entree: str):
         main()
 
 
-def trouverSuites(entree: str) -> dict:
+def trouverSousChainesRepetees(entree: str) -> list:
     """
-    entrée :
+    Entrée :
         entree (string) : la séquence ADN
-    sortie :
-        dictionnaire :
-            clé : suite de caractères
-            valeur : nombre d'occurence
-
-    précondition :
-        callback main si entrée trop courte
+    Sortie :
+        Liste : les sous-chaînes répétées dans la séquence ADN
     """
 
-    valDict = {}
+    sousChainesRepetees = []
 
-    tailleMinSequence = 2
-
-    if len(entree) < tailleMinSequence:
-        print("La séquence ADN est trop courte.")
-        main()
-
-    for i in range(tailleMinSequence, len(entree)):
+    for i in range(1, len(entree)):
         for j in range(0, len(entree) - i + 1):
-            valeur = entree[j : j + i]
-            if valeur in valDict:
-                valDict[valeur] += 1
-            else:
-                valDict[valeur] = 1
+            sousChaine = entree[j : j + i]
+            if sousChaine in entree[j + i :]:
+                sousChainesRepetees.append(sousChaine)
 
-    return valDict
+    return sousChainesRepetees
 
 
-def trouverMaxOccurences(valDict: dict) -> int:
+def trouverPlusLongueSousChaineRepetee(entree: str) -> str:
     """
-    entrée :
-        valDict (dictionnaire) : les différentes suites de nombres et leurs occurences
-    sortie :
-        int : la suite avec le plus d'occurences dans la séquence ADN
+    Entrée :
+        entree (string) : la séquence ADN
+    Sortie :
+        string : la plus longue sous-chaîne répétée dans la séquence ADN
     """
 
-    valeurMax = 0
+    sousChainesRepetees = trouverSousChainesRepetees(entree)
 
-    for key in valDict:
-        if valDict[key] > valeurMax:
-            valeurMax = valDict[key]
+    if not sousChainesRepetees:
+        return "Aucune sous-chaîne répétée trouvée."
 
-    return valeurMax
+    return max(sousChainesRepetees, key=len)
 
 
 def main():
     """
-    - obtenir séquence ADN via utilisateur
-    - vérifier input
-    - trouver les différentes suites
-    - trouver l'occurence maximale
-    - print la (ou les si plusieurs de même taille) plus longue suite(s) dans la séquence adn
+    - Obtenir séquence ADN via utilisateur
+    - Vérifier input
+    - Trouver la plus longue sous-chaîne répétée
+    - Print la plus longue sous-chaîne répétée dans la séquence ADN
     """
     entree = str(input("Veuillez entrer la séquence ADN (A-T-G-C): "))
     checkInput(entree)
 
-    valDict = trouverSuites(entree)
-    valeurMax = trouverMaxOccurences(valDict)
+    plusLongueSousChaine = trouverPlusLongueSousChaineRepetee(entree)
 
-    print(
-        "La valeur maximale est atteinte avec " + str(valeurMax) + " occurences par : "
-    )
-    for key in valDict:
-        if valDict[key] == valeurMax:
-            print("- " + key)
+    print("La plus longue sous-chaîne répétée est :", plusLongueSousChaine)
 
 
 if __name__ == "__main__":
     main()
+
+
+def test():
+    val = trouverSousChainesRepetees("AAACCT")
